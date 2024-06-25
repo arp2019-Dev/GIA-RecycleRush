3b257972a58dea6c114f4e58dd4272109640ebf8
Mihir Surlaker <74442956+Mihirsur007@users.noreply.github.com>
2024-06-25 12:15:24 -0700

    Created leaderboard EDIT REQUIRED
    The iboutlet in leaderboardviewcontroller.swift needs to be linked to the table in the leaderboard page

--- GIA-RecycleRush/LeaderBoardViewController.swift
+++ GIA-RecycleRush/LeaderBoardViewController.swift
@@ -3,11 +3,32 @@
 //  GIA-RecycleRush
 //
 //  Created by Akhil Raju on 6/24/24.
-//
+// used this page for the orange view - mihir
 
 import UIKit
 import FirebaseAuth
-class LeaderBoardViewController: UIViewController {
+class LeaderBoardViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var players: [Player] = [
+            Player(name: "Alice", score: 120),
+            Player(name: "Bob", score: 150),
+            Player(name: "Charlie", score: 90)
+        ]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LeaderboardCell", for: indexPath)
+                let player = players[indexPath.row]
+                cell.textLabel?.text = "\(player.name) - \(player.score)"
+                return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +40,9 @@ class LeaderBoardViewController: UIViewController {
             navigateToHomePage()
            
         }
+        tableView.dataSource = self
+                tableView.delegate = self
+                players.sort { $0.score > $1.score }
     }
     func navigateToHomePage() {
         performSegue(withIdentifier: "account", sender: nil)
---
+++ GIA-RecycleRush/Player.swift
@@ -0,0 +1,12 @@
+//
+//  Player.swift
+//  GIA-RecycleRush
+//
+//  Created by Mihir Surlaker on 6/25/24.
+//
+
+import Foundation
+struct Player {
+    let name: String
+    let score: Int
+}
